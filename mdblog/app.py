fd9e1170b44503c90fc1ac0d14870076f243c897
c0der0x <info@nethek.com>
2025-02-20 09:15:09 +0000

    add login session

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import session
+
+from .database import articles
 
 flask_app = Flask(__name__)
+flask_app.secret_key = b'\x9e\x04I\x9e\xaf*\x95\x94f\x90\x8b\x061g\x00dfG\xaa\xecn\x11]\xb0'
 
 @flask_app.route("/")
 def view_welcome_page():
@@ -13,12 +20,36 @@ def view_about():
 
 @flask_app.route("/admin/")
 def view_admin():
+    if "logged" not in session:
+        return redirect(url_for("view_login"))
     return render_template("admin.jinja")
 
 @flask_app.route("/articles/")
 def view_articles():
-    return render_template("articles.jinja")
+    return render_template("articles.jinja", articles=articles.items())
 
-@flask_app.route("/articles/<int:art_id>")
+@flask_app.route("/articles/<int:art_id>/")
 def view_article(art_id):
-    return render_template("article.jinja")
+    article = articles.get(art_id)
+    if article:
+        return render_template("article.jinja", article=article)
+    return render_template("article_not_found.jinja", art_id=art_id)
+
+@flask_app.route("/login/", methods=["GET"])
+def view_login():
+    return render_template("login.jinja")
+
+@flask_app.route("/login/", methods=["POST"])
+def login_user():
+    username = request.form["username"]
+    password = request.form["password"]
+    if username == "admin" and password == "admin":
+        session["logged"] = True
+        return redirect(url_for("view_admin"))
+    else:
+        return redirect(url_for("view_login"))
+
+@flask_app.route("/logout/", methods=["POST"])
+def logout_user():
+    session.pop("logged")
+    return redirect(url_for("view_welcome_page"))
